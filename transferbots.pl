fd80124137630ef961b838a65bd16d189d971f2c
m1ndgames <florian.luettgens@gmail.com>
2017-05-13 15:53:34 +0200

    update bot transfer script

--- transferbots.pl
+++ transferbots.pl
@@ -1,26 +1,39 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use Time::Piece;
 
 my $rsync = `which rsync`;
-my $path = '/home/t00r/RTB-Web/botuploads';
+my $uploadpath = '/home/t00r/RTB-Web/botuploads';
+my $botpath = '/home/rtb/bots';
+my $rtbserver = 'rtb@m1nd.io';
+my $sshport = 2222;
+my $date = localtime->strftime('%Y-%m-%d_%H:%M');
 
-opendir my $dir, $path or die "Cannot open directory: $!";
+opendir my $dir, $uploadpath or die "Cannot open directory: $!";
 my @files = readdir $dir;
 closedir $dir;
 
 foreach (@files) {
     if ($_ =~ /^\./) { next; }
     my $result;
-    my $scan = `clamscan --no-summary $path/$_`;
-    if ($scan =~ /$path\/$_: (.+)/) {
+    my $scan = `clamscan --no-summary $uploadpath/$_`;
+    if ($scan =~ /$uploadpath\/$_: (.+)/) {
         $result = $1;
     }
 
     if ($result ne 'OK') {
-        system("echo \"$_ is a Virus!\" >> bottransfer.log");
-        system("mv $path/$_ /home/t00r/RTB-Web/honeypot");
+        system("echo \"$date - $_ is a Virus!\" >> bottransfer.log");
+        system("mv $uploadpath/$_ /home/t00r/RTB-Web/honeypot");
     }
 }
 
-system("");
+my @rsync = `$rsync -avz -e \"ssh -p $sshport\" $uploadpath $rtbserver:$botpath`;
+
+foreach (@rsync) {
+	if ($_ =~ /^botuploads\/(.+)$/) {
+		my $upload = $1;
+		$date = localtime->strftime('%Y-%m-%d_%H:%M');
+		system("echo \"$date - Uploaded: $upload\" >> bottransfer.log");
+	}
+}
